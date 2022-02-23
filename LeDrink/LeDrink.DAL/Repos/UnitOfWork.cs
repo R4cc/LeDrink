a13a3041388959dced6c86a85e1729431b64fb24
Darkatek7 <heeltobias@gmail.om>
2022-02-23 11:06:18 +0100

    Replace = with ?? in UnitOfWork

--- LeDrink/LeDrink.DAL/Repos/UnitOfWork.cs
+++ LeDrink/LeDrink.DAL/Repos/UnitOfWork.cs
@@ -4,8 +4,8 @@ namespace LeDrink.DAL.Repos
 {
     public class UnitOfWork : IUnitOfWork
     {
-        private IDrinkRepo _drinkRepo;
+        private IDrinkRepo? _drinkRepo;
 
-        public IDrinkRepo DrinkRepo => _drinkRepo = new DrinRepo();
+        public IDrinkRepo DrinkRepo => _drinkRepo ?? new DrinRepo();
     }
 }
